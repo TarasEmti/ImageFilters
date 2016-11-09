34975eb979024eef6e6be94a57352df131d2ff81
Taras Emti <t-min@mail.ru>
2016-11-09 17:36:34 +0700

    Initial Commit

---
+++ SkillApp/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  SkillApp
+//
+//  Created by Тарас on 09.11.16.
+//  Copyright © 2016 Тарас. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ SkillApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SkillApp
+//
+//  Created by Тарас on 09.11.16.
+//  Copyright © 2016 Тарас. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
